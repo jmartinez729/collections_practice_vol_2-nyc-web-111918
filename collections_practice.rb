4fa0ca0ae8fb0c4faf4e8ea40a879553aae64bf2
John Martinez <jmartinez729@gmail.com>
2018-11-09 18:30:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,6 @@
 def begins_with_r(tools)
-  true_or_false = nil 
-  tools.each_with_index do |i|
-    if tools[i][0] == "r"
-      true_or_false == true
-    else 
-      true_or_false == false
-    end 
+  tools.all? do |tools|
+    
   end 
-  return true_or_false
 
 end 
